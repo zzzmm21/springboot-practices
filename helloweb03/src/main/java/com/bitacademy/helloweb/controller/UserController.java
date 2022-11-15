8f72550ff12726a2e17f6dbdd88f5de2d21e7d09
zzzmm21 <zzzmm21@gmail.com>
2022-11-15 11:33:11 +0900

    @RequestMapping &@RequestParam

--- helloweb03/src/main/java/com/bitacademy/helloweb/controller/UserController.java
+++ helloweb03/src/main/java/com/bitacademy/helloweb/controller/UserController.java
@@ -2,7 +2,10 @@ package com.bitacademy.helloweb.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * 
  * @RequestMapping 클래스 + 메소드 (추천)
@@ -11,14 +14,37 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/user")
 public class UserController {
-	@ResponseBody
-	@RequestMapping("/joinform")
-	public String joinfrom() {
-		return "UserController.joinform()";
+
+	@RequestMapping(value="/join", method=RequestMethod.GET)
+	public String join() {
+		return "/WEB-INF/views/join.jsp";
+	}
+
+	@RequestMapping(value="/join",method=RequestMethod.POST)
+	public String join(UserVo vo) {
+		System.out.println(vo);
+		return "redirect:/";
 	}
 	@ResponseBody
-	@RequestMapping("/join")
-	public String join() {
-		return "UserController.join()";
+	@RequestMapping("/update")
+	public String update(@RequestParam("n") String name) {
+		/** 
+		 * 만일 n이라는 이름의 URL 파라미터가 없는 경우
+		 * 400 bad Request 에러가 발생한다.
+		 */
+		return "UserController.update("+ name +")";
+	}
+	@ResponseBody
+	@RequestMapping("/update2")
+	public String update2(@RequestParam(value="n",required=true,defaultValue="" ) String name) {
+	
+		return "UserController.update("+ name +")";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/list")
+	public String list(@RequestParam(value="p",required=true,defaultValue="1" ) int pageNo) {
+	
+		return "UserController.update("+ pageNo +")";
 	}
 }
