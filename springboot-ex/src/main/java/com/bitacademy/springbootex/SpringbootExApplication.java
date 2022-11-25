a1709d223882f678e5d05a1d7a2b0d084a62d6f9
zzzmm21 <zzzmm21@gmail.com>
2022-11-25 15:05:23 +0900

    springboot-helloworld

---
+++ springboot-ex/src/main/java/com/bitacademy/springbootex/SpringbootExApplication.java
@@ -0,0 +1,13 @@
+package com.bitacademy.springbootex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootExApplication.class, args);
+	}
+
+}
---
+++ springboot-ex/src/test/java/com/bitacademy/springbootex/SpringbootExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitacademy.springbootex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
